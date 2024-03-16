babd657e0e01167f25be815914a0f0805d037253
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-03-16 21:18:13 +0200

    Update Cart

--- Esty-Models/Cart.cs
+++ Esty-Models/Cart.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace Esty_Models
 {
-    public class CustomerProduct
+    public class Cart
     {
+        public int CartID { get; set; }
+
+        public Products? products { get; set; }
         public int ProductId { get; set; }
 
+        public Customer? customer { get; set; }
         public int CustomerId { get; set; }
 
         public int Quantity { get; set; }
