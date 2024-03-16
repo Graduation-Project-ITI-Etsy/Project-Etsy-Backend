0be193cda69ddd18ed3682a6d7ffdf19c428413c
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-03-16 17:05:16 +0200

    Add CustomerProduct Model

---
+++ Esty-Models/CustomerProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esty_Models
+{
+    public class CustomerProduct
+    {
+        public int ProductId { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
