9ade5026e932929e50bd9986fd8e2c0383b90349
ZIAD <ZIAD@ZIAD-PC>
2024-03-16 21:32:59 +0200

    Add Models Add Context

---
+++ Esty-Context/Configration/BaseCategoryConfigration.cs
@@ -0,0 +1,26 @@
+﻿using Esty_Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esty_Context.Configration
+{
+    public class BaseCategoryConfigration : IEntityTypeConfiguration<BaseCategory>
+    {
+        public void Configure(EntityTypeBuilder<BaseCategory> builder)
+        {
+
+            builder.HasKey(bc => bc.Id);
+            builder.Property(bc => bc.Name).IsRequired().HasMaxLength(100);
+
+            builder.HasMany(bc => bc.Categories)
+                .WithOne(c => c.BaseCategory)
+                .HasForeignKey(c => c.BaseCategoryId);
+        }
+
+    }
+}
