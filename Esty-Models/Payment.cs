16f4e1650c1a96d8306638ebeb7836c30651081e
solwanmahmoud <117037484+solwanmahmoud@users.noreply.github.com>
2024-03-16 20:39:48 +0200

    payment model push by solwan first commit

---
+++ Esty-Models/Payment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esty_Models
+{
+    public class Payment
+    {
+        public int PaymentID { get; set; }
+        public double TotalPrice { get; set; }
+        public string Response { get; set; }
+
+        public int CustomerId { get; set; }
+
+        // Foreign key property
+        public int OrderId { get; set; }
+        // Navigation property
+        //public Order Order { get; set; }
+    }
+}
