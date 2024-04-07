98af0aa9919f39940a3dcb01a74b93aeb64e2ba9
Zyad <ziadhakem7@gmail.com>
2024-04-08 00:18:39 +0200

    Add Localization Basic & Test in BaseCategory Done Ya5waty ^-^

--- Esty-Presentation/Controllers/BaseCategoryController.cs
+++ Esty-Presentation/Controllers/BaseCategoryController.cs
@@ -1,22 +1,48 @@
 ﻿using Esty_Applications.Services.BaseCategory;
+using Esty_Presentation.Models;
 using Etsy_DTO.BaseCategory;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
 
 namespace Esty_Presentation.Controllers
 {
     public class BaseCategoryController : Controller
     {
         private readonly IBaseCategoryServices _baseCategoryServices;
-
-        public BaseCategoryController(IBaseCategoryServices baseCategoryServices)
+        private readonly IStringLocalizer<BaseCategoryController> _localizer;
+        public BaseCategoryController(IBaseCategoryServices baseCategoryServices, IStringLocalizer<BaseCategoryController> localizer)
         {
             _baseCategoryServices = baseCategoryServices;
+            _localizer = localizer;
         }
 
         public async Task<IActionResult> Index()
         {
+            ViewBag.Zyad = _localizer[name: "zyad"];
+            ViewBag.NameEN = _localizer[name: "NameEN"];
             var result = await _baseCategoryServices.GetAllBaseCategory();
             return View(result);
+        }
+
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+            return LocalRedirect(returnUrl);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         public IActionResult Create()
