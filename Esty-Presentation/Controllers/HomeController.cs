e849e4cf80ab32143c95c47e2e14fd68c953e9aa
Zyad <ziadhakem7@gmail.com>
2024-04-12 09:54:53 +0200

    Adding Localization in Charts Done ○_○

--- Esty-Presentation/Controllers/HomeController.cs
+++ Esty-Presentation/Controllers/HomeController.cs
@@ -19,21 +19,38 @@ namespace Esty_Presentation.Controllers
         private readonly IProductsServices _productsServices;
         private readonly ICategoryServices _categoryServices;
         private readonly IPaymentServices _paymentServices;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
 
         public HomeController(IOrderServices orderServices
             ,IProductsServices productsServices
             ,IBaseCategoryServices baseCategoryServices
             ,ICategoryServices categoryServices
-            ,IPaymentServices paymentServices)
+            ,IPaymentServices paymentServices
+            , IStringLocalizer<HomeController> localizer)
         {
             _orderServices = orderServices;
             _baseCategoryServices = baseCategoryServices;
             _productsServices = productsServices;
             _categoryServices = categoryServices;
             _paymentServices = paymentServices;
+            _localizer = localizer;
+
         }
         public async Task<IActionResult> Index()
         {
+            //Localization
+            ViewBag.countBC = _localizer[name: "countBC"];
+            ViewBag.countC = _localizer[name: "countC"];
+            ViewBag.CountP = _localizer[name: "CountP"];
+            ViewBag.countO = _localizer[name: "countO"];
+            ViewBag.countPay = _localizer[name: "countPay"];
+            ViewBag.countPayP = _localizer[name: "countPayP"];
+            ViewBag.countCP = _localizer[name: "countCP"];
+            ViewBag.countOD = _localizer[name: "countOD"];
+
+
+
             ViewBag.BaseCategoryCount = await _baseCategoryServices.BaseCategoryCount();
             ViewBag.CategoryCount = await _categoryServices.CategoryCount();
             ViewBag.ProductCount = await _productsServices.ProductCount();
